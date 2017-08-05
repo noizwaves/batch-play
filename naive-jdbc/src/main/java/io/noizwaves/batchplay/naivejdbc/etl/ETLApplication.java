f4a7d148315c16f81a742ca80867122495765fbf
Adam Neumann <noizwaves@users.noreply.github.com>
2017-08-05 13:02:41 -0600

    Initial commit of app shell.

---
+++ naive-jdbc/src/main/java/io/noizwaves/batchplay/naivejdbc/etl/ETLApplication.java
@@ -0,0 +1,12 @@
+package io.noizwaves.batchplay.naivejdbc.etl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ETLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ETLApplication.class, args);
+	}
+}
---
+++ naive-jdbc/src/test/java/io/noizwaves/batchplay/naivejdbc/etl/ETLApplicationTests.java
@@ -0,0 +1,16 @@
+package io.noizwaves.batchplay.naivejdbc.etl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ETLApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
